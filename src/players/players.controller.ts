cd5c4d9109512c905f625ed2aec150cc5485d764
Drinnn <pedro_lontro@hotmail.com>
2021-01-05 16:33:28 -0300

    feat: add validation pipe

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { PlayerCreateDto } from './dtos/player.create.dto';
 import { Player } from './interfaces/player.interface';
 import { PlayersService } from './players.service';
@@ -8,6 +17,7 @@ export class PlayersController {
   constructor(private readonly playersService: PlayersService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   async createUpdate(@Body() playerCreateDto: PlayerCreateDto) {
     await this.playersService.createUpdate(playerCreateDto);
   }
