f6ac79a7ab6fe1c12190d1fb8d0ec6b803a8cbea
Drinnn <pedro_lontro@hotmail.com>
2021-01-15 08:45:47 -0300

    feat: add update and assign player to category services

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -4,11 +4,13 @@ import {
   Get,
   Param,
   Post,
+  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dtos/categories.create.dto';
+import { UpdateCategoryDto } from './dtos/categories.update.dto';
 import { Category } from './interfaces/categories.interface';
 
 @Controller('categories')
@@ -29,5 +31,19 @@ export class CategoriesController {
   @Get('/:name')
   async getByName(@Param('name') name: string): Promise<Category> {
     return await this.categoriesService.getByName(name);
+  }
+
+  @Put('/:name')
+  @UsePipes(ValidationPipe)
+  async update(
+    @Body() updateDto: UpdateCategoryDto,
+    @Param('name') name: string,
+  ): Promise<void> {
+    return await this.categoriesService.update(name, updateDto);
+  }
+
+  @Post('/:name/players/:playerId')
+  async assignPlayer(@Param() params: string[]): Promise<void> {
+    return await this.categoriesService.assignPlayer(params);
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -5,13 +5,16 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { PlayersService } from 'src/players/players.service';
 import { CreateCategoryDto } from './dtos/categories.create.dto';
+import { UpdateCategoryDto } from './dtos/categories.update.dto';
 import { Category } from './interfaces/categories.interface';
 
 @Injectable()
 export class CategoriesService {
   constructor(
     @InjectModel('Category') private readonly categoryModel: Model<Category>,
+    private readonly playersService: PlayersService,
   ) {}
 
   async create(createDto: CreateCategoryDto): Promise<Category> {
@@ -28,7 +31,7 @@ export class CategoriesService {
   }
 
   async getAll(): Promise<Array<Category>> {
-    return await this.categoryModel.find().exec();
+    return await this.categoryModel.find().populate('players').exec();
   }
 
   async getByName(name: string): Promise<Category> {
@@ -39,5 +42,54 @@ export class CategoriesService {
     }
 
     return foundedCategory;
+  }
+
+  async update(name: string, updateDto: UpdateCategoryDto): Promise<void> {
+    const foundedCategory = await this.categoryModel.findOne({ name }).exec();
+
+    if (!foundedCategory) {
+      throw new NotFoundException(`Category ${name} doesn't exists.`);
+    }
+
+    await this.categoryModel
+      .findOneAndUpdate({ name }, { $set: updateDto })
+      .exec();
+  }
+
+  async assignPlayer(params: string[]): Promise<void> {
+    const categoryName = params['name'];
+    const playerId = params['playerId'];
+
+    const foundedCategory = await this.categoryModel
+      .findOne({ name: categoryName })
+      .exec();
+
+    if (!foundedCategory) {
+      throw new BadRequestException(`Category ${categoryName} doesn't exists.`);
+    }
+
+    const foundedPlayer = await this.playersService.getById(playerId);
+
+    if (!foundedPlayer) {
+      throw new BadRequestException(`Player with ID ${playerId} not found.`);
+    }
+
+    const playerAlreadyInCategory = await this.categoryModel
+      .find({ name: categoryName })
+      .where('players')
+      .in(playerId)
+      .exec();
+
+    if (!!playerAlreadyInCategory) {
+      throw new BadRequestException(
+        `Player ${foundedPlayer.name} already on Category ${categoryName}.`,
+      );
+    }
+
+    foundedCategory.players.push(playerId);
+
+    await this.categoryModel
+      .findOneAndUpdate({ name: categoryName }, { $set: foundedCategory })
+      .exec();
   }
 }
